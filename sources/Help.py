a3663940462f3af305e8504d55d43565c303fdee
tientran0826 <79437149+tientran0826@users.noreply.github.com>
2022-06-02 19:31:32 +0700

    add

--- sources/Help.py
+++ sources/Help.py
@@ -3,14 +3,33 @@ from discord.ext import commands,tasks
 
 
 help_content = """
-  ```connect - Kết nối voice chat
-disconnect - Ngắt kết nối từ voice chat
-play - Phát nhạc
-pause - Tạm dừng
-resume - Tiếp tục
-nowplaying - Bài hát đang được chơi
-queue - Xem playlist hiện tại
-loop - Lặp lại bài hát/playlist```
+==============================================
+  ___  ___                        __    ___ 
+  |  \/  |                       /  |  /   |
+  | .  . |_   _  ___  _ __   __ _`| | / /| |
+  | |\/| | | | |/ _ \| '_ \ / _` || |/ /_| |
+  | |  | | |_| | (_) | | | | (_| || |\___  |
+  \_|  |_/\__,_|\___/|_| |_|\__, \___/   |_/
+                            __/ |          
+                            |___/   
+================================================
+*Hệ thống*
+- setprefix + list() : thay đổi prefix mặc định
+*Anime*
+- animer: Random ảnh anime
+- animegif: Random ảnh anime dạng .gif
+- cry: khóc
+- kiss: Hun
+- animes: Tìm tên anime dựa trên ảnh hoặc video
+*Rút gọn link*
+- tiny : Rút gọn link sang tinyurl
+*Random món ăn*
+- fadd: Thêm món vào list
+- fshow: Danh sách món đã thêm
+- frandom: Lựa chọn bất kỳ s1 chón 1 món trong danh sách 
+- fclear: Xoá danh sách vừa tạo
+*Ngoài lề*
+- ck: Chửi chó khoa :>
 """
 
 class Help(commands.Cog):
@@ -19,4 +38,4 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self,ctx):
-        await ctx.send(help_content)
+        await ctx.send("```"+ help_content + "```")
